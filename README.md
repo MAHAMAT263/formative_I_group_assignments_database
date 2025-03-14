2ed660ddec19c0c159fd147327c608f7da118925
MAHAMAT263 <m.ahmat@alustudent.com>
2025-03-14 17:38:51 +0100

    mahamat

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-Database assignment
+Database 
--- SQL/Sqldatabasecreation.py
+++ SQL/Sqldatabasecreation.py
@@ -27,7 +27,7 @@ DB_CONFIG = {
     "user": os.getenv("DB_USER"),
     "password": os.getenv("DB_PASSWORD"),
     "database": os.getenv("DB_NAME"),
-    "port": int(os.getenv("DB_PORT"))
+    "port": int(os.getenv("3306"))
 }
 
 

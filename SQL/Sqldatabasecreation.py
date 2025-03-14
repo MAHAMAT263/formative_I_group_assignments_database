9b7acf25f1a002702734e742789e9181ac39f20c
MAHAMAT263 <m.ahmat@alustudent.com>
2025-03-14 20:05:44 +0100

    mahamat

--- SQL/Sqldatabasecreation.py
+++ SQL/Sqldatabasecreation.py
@@ -23,7 +23,7 @@ load_dotenv()
 
 # Database connection details
 DB_CONFIG = {
-    "host": os.getenv("DB_HOST" , "sql12.freesqldatabase.com"),
+    "host": os.getenv("DB_HOST"),
     "user": os.getenv("DB_USER"),
     "password": os.getenv("DB_PASSWORD"),
     "database": os.getenv("DB_NAME"),

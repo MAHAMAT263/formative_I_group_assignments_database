96a4b16797da5000a8442524f2854bfce4250fdf
MAHAMAT263 <m.ahmat@alustudent.com>
2025-03-14 17:47:41 +0100

    Mahamat

--- SQL/Sqldatabasecreation.py
+++ SQL/Sqldatabasecreation.py
@@ -27,7 +27,7 @@ DB_CONFIG = {
     "user": os.getenv("DB_USER"),
     "password": os.getenv("DB_PASSWORD"),
     "database": os.getenv("DB_NAME"),
-    "port": int(os.getenv("3306"))
+    "port": int(os.getenv("DB_PORT", "3306"))  # Uses "3306" as a default value
 }
 
 
